b2e16a91de62a088bfca336990c4bc20b55a0ba1
edu-ofernandes <eduardo123fernandes2013@hotmail.com>
2019-12-04 04:53:47 -0300

    continuidade no jogo feito com phonegap na aula do Nilo, quase completo parte visual

--- Uc-9/aula-8/jogoPhongap/GamePhonegap/www/js/logica.js
+++ Uc-9/aula-8/jogoPhongap/GamePhonegap/www/js/logica.js
@@ -1,3 +1,21 @@
-function executar() {
-    alert("foooiii ");
+function reset() {
+    window.location.reload();
 }
+function darkMode() {
+    alert("darkMode ");
+}
+function startGame() {
+    document.documentElement.querySelector(".startGame").addEventListener("click", function(){
+        const conteudo = document.documentElement.querySelector(".conteudo").classList;
+        const footer = document.documentElement.querySelector(".footer").classList;
+        if(conteudo.contains("disable") || footer.contains("disable")){
+            conteudo.add("active");
+            conteudo.remove("disable");
+            footer.add("active");
+            footer.remove("disable");
+            document.querySelector(".comeco").classList.add("disable");
+        }
+        
+    })
+}
+startGame();
