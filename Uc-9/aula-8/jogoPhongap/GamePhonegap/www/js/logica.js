16fbc25526aa63e4d429b6578af3666ea4b13dcd
edu-ofernandes <eduardo123fernandes2013@hotmail.com>
2019-12-07 21:04:57 -0300

    ajustes no jogo de perguntas feito com phonegap

--- Uc-9/aula-8/jogoPhongap/GamePhonegap/www/js/logica.js
+++ Uc-9/aula-8/jogoPhongap/GamePhonegap/www/js/logica.js
@@ -29,9 +29,9 @@ const perguntas = [
     }
 ]
 
-const conteudo = document.documentElement.querySelector(".conteudo");
-const footer = document.documentElement.querySelector(".footer");
-const comeco = document.documentElement.querySelector(".comeco");
+const conteudo = document.querySelector(".conteudo");
+const footer = document.querySelector(".footer");
+const comeco = document.querySelector(".comeco");
 
 
 
@@ -49,30 +49,65 @@ function trocarParaTelaJogo() {
             
             comeco.classList.remove("active");
             comeco.classList.add("disable");
-            startGame();
-            temporizador();
+            startGame(perguntas);
+            
         }
   
         
     })
 }
 
+function questaoCorreta() {
+    document.querySelector(".resposta").addEventListener("click", function(){
+        console.log(this)
+    })
+}
 
-function startGame(){
-    for(let i = 0; i < perguntas.length; i++){
-        setInterval(function(){
-            console.log("pergunta: " + perguntas[i].pergunta)
-            document.documentElement.querySelector(".perguntah2").innerText = perguntas[i].pergunta;
-        }, perguntas[0].tempo * 1000);
+function trocarPerguntaResposta(i){
+    document.documentElement.querySelector(".perguntah2").innerText = perguntas[i].pergunta;
+    document.documentElement.querySelector(".resposta").innerText = perguntas[i].resposta;
+
+    questaoCorreta();
+}
+
+function startGame(perguntas){
+    // for(let i = 0; i < perguntas.length; i++){
+        let i = 0;
+        trocarPerguntaResposta(i);
+        console.log("pergunta: " + perguntas[i].pergunta)
+        console.log(i)
+        temporizador();
+        const meuIntervalo = setInterval(function(){
+            trocarPerguntaResposta(i+1);
+            console.log("pergunta: " + perguntas[i+1].pergunta)
+            console.log(i+ 1)            
+            
+            i++;
+            if(i >= perguntas.length -1){
+                clearInterval(meuIntervalo)
+                finalJogo();
+            }
+            
+        }, (perguntas[0].tempo * 1000) + 2000);
         
-    }
-    
+        
+    // }
 
 }
 
 function temporizador(){
-    setInterval(function(){
-        document.documentElement.querySelector("h3").innerHTML = '<i class="fas fa-hourglass-start"></i>: 00:0' + perguntas[0].tempo--;
+    let tempo = perguntas[0].tempo ;
+    let h3Relogio = document.querySelector("h3").innerHTML = '<i class="fas fa-hourglass-start"></i>: 00:';
+
+    (tempo.toString().length < 2) ? document.querySelector("h3").innerHTML = '<i class="fas fa-hourglass-start"></i>: 00:0' + tempo :  document.querySelector("h3").innerHTML = '<i class="fas fa-hourglass-start"></i>: 00:' + tempo;
+
+    const meuTemporizador = setInterval(function(){
+        (tempo.toString().length < 2) ? document.querySelector("h3").innerHTML = '<i class="fas fa-hourglass-start"></i>: 00:0' + tempo-- :  document.querySelector("h3").innerHTML = '<i class="fas fa-hourglass-start"></i>: 00:' + tempo-- ;
+
+        if(tempo < 0){
+            clearInterval(meuTemporizador)
+            console.log("temporizador parado")
+        }
     }, 1000);
 }
 
@@ -93,6 +128,6 @@ function reset() {
 
 
 function darkMode() {
-    document.documentElement.querySelector('body').classList.toggle("dark");
-    document.documentElement.querySelector('h3').classList.toggle("dark");
+    document.querySelector('body').classList.toggle("dark");
+    document.querySelector('h3').classList.toggle("dark");
 }
