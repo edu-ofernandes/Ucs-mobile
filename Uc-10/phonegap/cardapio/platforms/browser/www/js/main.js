28b0b50fb602499858fde31a7465ba94f41a0529
edu-ofernandes <eduardo123fernandes2013@hotmail.com>
2020-03-04 21:21:27 -0300

    alteraçoes no app de cardapio com phonegap,criado algumas telas para login entre outros

--- Uc-10/phonegap/cardapio/platforms/browser/www/js/main.js
+++ Uc-10/phonegap/cardapio/platforms/browser/www/js/main.js
@@ -1,68 +1,71 @@
 const btnPedido = document.querySelector('.itens .info .btnMain');
-btnPedido.addEventListener('click', () => window.location.assign('itemInfo.html'));
 const btnEntrar = document.querySelector('#login');
-btnEntrar.addEventListener('click', () => window.location.assign('entrar.html'));
-
-
+const itensHtml = document.querySelectorAll('.itens');
 
 const lanche = [
   {
     nome: "Nome da comida1",
     descricao: "descricao da comida1",
     preco: 25.00,
-    foto: "./img/pizza.jpg"
+    foto: ["./img/pizza.jpg", "./img/pizza.jpg", "./img/pizza.jpg"],
   },
   {
     nome: "Nome da comida2",
     descricao: "descricao da comida2",
     preco: 25.00,
-    foto: "./img/pizza.jpg"
+    foto: ["./img/pizza.jpg", "./img/pizza.jpg", "./img/pizza.jpg"],
   },
   {
     nome: "Nome da comida3",
     descricao: "descricao da comida3",
     preco: 25.00,
-    foto: "./img/pizza.jpg"
+    foto: ["./img/pizza.jpg", "./img/pizza.jpg", "./img/pizza.jpg"],
   },
   {
     nome: "Nome da comida4",
     descricao: "descricao da comida4",
     preco: 25.00,
-    foto: "./img/pizza.jpg"
+    foto: ["./img/pizza.jpg", "./img/pizza.jpg", "./img/pizza.jpg"],
   },
   {
     nome: "Nome da comida5",
     descricao: "descricao da comida5",
     preco: 25.00,
-    foto: "./img/pizza.jpg"
+    foto: ["./img/pizza.jpg", "./img/pizza.jpg", "./img/pizza.jpg"],
   },
   {
     nome: "Nome da comida6",
     descricao: "descricao da comida6",
     preco: 25.00,
-    foto: "./img/pizza.jpg"
+    foto: ["./img/pizza.jpg", "./img/pizza.jpg", "./img/pizza.jpg"],
   },
   {
     nome: "Nome da comida7",
     descricao: "descricao da comida7",
     preco: 25.00,
-    foto: "./img/pizza.jpg"
+    foto: ["./img/pizza.jpg", "./img/pizza.jpg", "./img/pizza.jpg"],
   },
   {
     nome: "Nome da comida8",
     descricao: "descricao da comida8",
     preco: 25.00,
-    foto: "./img/pizza.jpg"
+    foto: ["./img/pizza.jpg", "./img/pizza.jpg", "./img/pizza.jpg"],
   },
 ];
 
-const itensHtml = document.querySelectorAll('.itens');
+
 itensHtml.forEach((item, index) => {
   let imgHtml = item.querySelector('img');
   let h3Html = item.querySelector('h3');
   let h5Html = item.querySelector('h5');
 
-  imgHtml.setAttribute('src', lanche[index].foto);
-  h3Html.insertAdjacentText("afterbegin", lanche[index].nome);
-  h5Html.insertAdjacentText("afterbegin", lanche[index].descricao);
-})
+  imgHtml.setAttribute('src', lanche[index].foto[0]);
+  h3Html.insertAdjacentText('afterbegin', lanche[index].nome);
+  h5Html.insertAdjacentText('afterbegin', lanche[index].descricao);
+});
+
+const cadastrarHtml = document.querySelector('.cadastrar');
+const entrarHtml = document.querySelector('.entrar');
+
+
+
